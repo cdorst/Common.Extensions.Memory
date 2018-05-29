45d56c530c5f805b2d6ba19f94ab7168a26d369a
Christopher Dorst <chris@dorst.email>
2018-05-29 18:40:22 -0500

    Add GetSpan methods

--- Common.Extensions.Memory/IntegerSpanExtensions.cs
+++ Common.Extensions.Memory/IntegerSpanExtensions.cs
@@ -4,6 +4,15 @@ namespace Common.Extensions.Memory
 {
     public static class IntegerSpanExtensions
     {
+        public static ReadOnlySpan<byte> GetSpan(in Int16 value)
+            => new Int16ByteMap(in value).AsReadOnlySpan();
+
+        public static ReadOnlySpan<byte> GetSpan(in Int32 value)
+            => new Int32ByteMap(in value).AsReadOnlySpan();
+
+        public static ReadOnlySpan<byte> GetSpan(in Int64 value)
+            => new Int64ByteMap(in value).AsReadOnlySpan();
+
         public static ReadOnlySpan<byte> ToSpan(this Int16 value)
             => new Int16ByteMap(in value).AsReadOnlySpan();
 
